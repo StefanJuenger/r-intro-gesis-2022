84627863182ee4c0a9226da36d2eb62ebaae7027
Stefan Juenger <stefan.juenger@gesis.org>
2022-08-12 12:53:34 +0200

    script to install & load all packages to code along

---
+++ my_scripts/package_installation_loading.R
@@ -0,0 +1,32 @@
+# check whether the package "easypackages" is installed: if not, install & load
+# it; if yes, just load it
+if (!require("easypackages")) {
+  install.packages("easypackages")
+  library(easypackages)
+  } else {
+  library(easypackages)
+}
+
+# load (hopefully...) all packages of the course to code along
+packages(
+  "broom",
+  "dplyr",
+  "ggplot2",
+  "haven",
+  "marginaleffects",
+  "naniar",
+  "parameters",
+  "patchwork",
+  "performance",
+  "randomNames",
+  "readr",
+  "readxl",
+  "remotes",
+  "report",
+  "see",
+  "sjlabelled",
+  "sjPlot",
+  "stargazer",
+  "tibble",
+  "tidyverse"
+)
